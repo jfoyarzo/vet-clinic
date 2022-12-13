267a80c1afdcdc25d331b4b300250778fecd8bfe
jfoyarzo <foyarzo621@gmail.com>
2022-12-12 21:11:15 -0300

    Update data.sql with the correct value for date_of_birth of Agumon

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
  INSERT INTO animals VALUES (1, 'Agumon', '2022-02-03', 0, true, 10.23);
- INSERT INTO animals VALUES (2, 'Gabumon', '2020-11-15', 2, true, 8);
+ INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
  INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
  INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
