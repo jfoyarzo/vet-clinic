e24af687067b2a109010ec88358e0d386f8d3593
jfoyarzo <foyarzo621@gmail.com>
2022-12-14 17:58:39 -0300

    Update schema.sql file with queries for the creation of 2 new tables and modify the existing one

--- schema.sql
+++ schema.sql
@@ -12,3 +12,29 @@ CREATE TABLE animals(
     );
 
 ALTER TABLE animals ADD species varchar(30);
+
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50),
+    age INT,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+    );
+
+ALTER TABLE animals
+ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY 
+(START WITH 11);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_species_id
+FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner_id
+FOREIGN KEY (owner_id) REFERENCES owners(id);
